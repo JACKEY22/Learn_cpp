24ce0e29750be0a887a34e547440dc3523ee57c7
jakey22 <sunjeon7@gmail.com>
2020-11-17 09:12:53 +0900

    11/17

--- FunctionCallbyReference.cpp
+++ FunctionCallbyReference.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 void swap(int x, int y) {
-    int temp; 
-    temp = x;  
-    x = y; 
-    y = temp;
+    int temp;                           
+    temp = x;                       // temp = 100
+    x = y;                          // x = 200;
+    y = temp;               
+    cout << &x << endl;
 }
 
 void swapbyreference(int &x, int &y) {
@@ -13,12 +14,13 @@ void swapbyreference(int &x, int &y) {
     temp = x;  
     x = y; 
     y = temp;
+    cout << &x << endl;
 }
 int main() {
     int first = 100, second = 200;
-    cout << "Before swap - first, second : " << first << "," << second << endl;
+    cout << "Before swap - first, second : " << first << "," << second << " &first : " << &first << endl;
     swap(first, second);
-    cout << "After swap - first, second : " << first << "," << second << endl;
+    cout << "After swap - first, second : " << first << "," << second << " &first : " << &first << endl;
 
     swapbyreference(first, second);
     cout << "Afrer swap by reference - first, second : " << first << "," << second << endl;
