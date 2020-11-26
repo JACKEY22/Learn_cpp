fd317c213d05a4a07d9621344126277482afeeac
jakey22 <sunjeon7@gmail.com>
2020-11-26 18:33:38 +0900

    11/26

---
+++ Class.cpp
@@ -0,0 +1,40 @@
+class BOX
+{
+private:
+    /* data */
+public:
+    BOX(/* args */);
+    ~BOX();
+};
+
+BOX::BOX(/* args */)
+{
+}
+
+BOX::~BOX()
+{
+}
+
+class box2
+
+{
+private:
+    /* data */
+public:
+    box2
+    (/* args */);
+    ~box2
+    ();
+};
+
+box2
+::box2
+(/* args */)
+{
+}
+
+box2
+::~box2
+()
+{
+}
